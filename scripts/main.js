f8d2f70c581fd0c832a37c2d0622a7e0016d225e
alex <boomscrip@gmail.com>
2019-03-10 13:39:27 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// Show additional video-blocks
+
+function viewDiv(){
+  document.getElementById("record__wrapp").style.display = "flex";
+};
+
+
+
+	$('.button__record').click(function(){
+		
+		$('.record__wrapp').css('display', 'flex');
+
+	});
